62c3ebb6354dce9612d8cf448176841e4156056f
Padliwinata <rpadliwinata@gmail.com>
2024-09-25 20:43:01 +0700

    initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="supply_chain",
+    packages=find_packages(exclude=["supply_chain_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "faker"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
---
+++ supply_chain/resources.py
@@ -0,0 +1,8 @@
+from dagster import ConfigurableResource
+from faker import Faker
+
+faker = Faker()
+
+
+
+
